69f328989d9fde4b629099643335d40e27015cf4
Mariusz Lyszczarz <lyszczarz.mariusz@gmail.com>
2019-06-23 09:26:51 +0200

    Initial commit

---
+++ src/main/java/lyszczarz/mariusz/pl/ArduinoWeather/ArduinoWeatherApplication.java
@@ -0,0 +1,13 @@
+package lyszczarz.mariusz.pl.ArduinoWeather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArduinoWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArduinoWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lyszczarz/mariusz/pl/ArduinoWeather/ArduinoWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package lyszczarz.mariusz.pl.ArduinoWeather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArduinoWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
